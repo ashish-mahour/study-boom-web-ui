dcc14b9ed6255e0dd8624dea51e7af963e6bf48f
Ashish Kumar Mahour <amashishmahour@gmail.com>
2019-06-30 01:31:38 +0530

    EDIT ADMIN COMPLETED!!!

--- src/app/authentication/edit-users/admin-edit-details/admin-edit-details.component.ts
+++ src/app/authentication/edit-users/admin-edit-details/admin-edit-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-edit-details',
@@ -8,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class AdminEditDetailsComponent implements OnInit {
 
   userType: string = 'ADMIN';
-  
-  constructor() { }
+
+  adminRegisterForm: FormGroup = this.formBuilder.group({
+    fullname: [null, [Validators.required, Validators.pattern("[A-Za-z ]+")]],
+    email: [null, [Validators.required, Validators.pattern("[A-Za-z0-9]+[@][A-Za-z]+[.][A-Za-z]{2,3}")]],
+    username: [null, [Validators.required, Validators.pattern("[A-Za-z0-9]+")]],
+    password: [null, [Validators.required]]
+  })
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+  }
+
+  editProfile(){
+    
   }
 
 }
